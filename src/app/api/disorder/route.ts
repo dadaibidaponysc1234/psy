f84f1c5203282307470dbfdf69527204665d7aec
Skeby <103804946+Skeby@users.noreply.github.com>
2024-10-04 14:27:00 +0100

    update: disorder api call

--- src/app/api/disorder/route.ts
+++ src/app/api/disorder/route.ts
@@ -1,21 +1,25 @@
 import axios from "axios";
+import dynamic from "next/dynamic";
 import { NextRequest, NextResponse } from "next/server";
 
- 
-
-
-
 export async function GET(req: NextRequest) {
-    try {
+  try {
+    const url =
+      "https://algorithmxcomp.pythonanywhere.com/api/disorder-study-count/";
+    const response = await axios.get(url);
 
-      const url =  'https://algorithmxcomp.pythonanywhere.com/api/disorder-study-count/' ;
-        const response = await axios.get(url);
-        // console.log(response);
-        
-        return NextResponse.json(response.data);
-        
-    } catch (error) {
-        console.log("Error fetching data");
-        return new Response("Error fetching studying view list", { status: 500 });
-    }
+    const nextResponse = NextResponse.json(response.data);
+    nextResponse.headers.set(
+      "Cache-Control",
+      "no-store, no-cache, must-revalidate, proxy-revalidate"
+    );
+    nextResponse.headers.set("Pragma", "no-cache");
+    nextResponse.headers.set("Expires", "0");
+    // console.log(response);
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.log("Error fetching data");
+    return new Response("Error fetching studying view list", { status: 500 });
+  }
 }
--- src/types/genetic.ts
+++ src/types/genetic.ts
@@ -2,4 +2,5 @@ export interface Region {
   genetic_source_materials__material_type: string;
   study_count: number;
   color: string;
+  source: number;
 }
