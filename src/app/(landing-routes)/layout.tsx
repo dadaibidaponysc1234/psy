42a2a6239c67f7662566de9f183a4693f341a335
Oladeji Omolabake <oladejiomolabake14@Gmail.com>
2025-05-18 00:27:45 +0100

    first commit

--- src/app/(landing-routes)/layout.tsx
+++ src/app/(landing-routes)/layout.tsx
@@ -1,15 +1,22 @@
-import Footer from "@/components/footer";
-import NavBar from "@/components/nav";
-import { ReactNode } from "react";
+"use client"
+import Footer from "@/components/footer"
+import NavBar from "@/components/nav"
+import { usePathname } from "next/navigation"
+import { ReactNode } from "react"
 
 const LandingLayout = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname()
   return (
     <>
       <NavBar />
-      <div className="flex flex-col min-h-dvh flex-grow">{children}</div>
-      <Footer />
+      <div
+        className={`flex ${pathname === "/OpoloAI" ? "h-screen overflow-hidden" : "min-h-dvh"} flex-grow flex-col`}
+      >
+        {children}
+      </div>
+      {pathname !== "/OpoloAI" && <Footer />}
     </>
-  );
-};
+  )
+}
 
-export default LandingLayout;
+export default LandingLayout
--- src/static/index.ts
+++ src/static/index.ts
@@ -1,8 +1,8 @@
-export const APP_NAME = "PsychGenAfrica";
-export const BASE_URL = "https://algorithmxcomp.pythonanywhere.com/api";
-export const AUTH_TOKEN = "psychgen-auth-token";
-export const REFRESH_TOKEN = "psychgen-refresh-token";
-export const API_COOKIE = "csrftoken";
+export const APP_NAME = "PsychGenAfrica"
+export const BASE_URL = "https://algorithmxcomp.pythonanywhere.com/api"
+export const AUTH_TOKEN = "psychgen-auth-token"
+export const REFRESH_TOKEN = "psychgen-refresh-token"
+export const API_COOKIE = "csrftoken"
 
 export const navItems: { name: string; path: string }[] = [
   {
@@ -21,6 +21,10 @@ export const navItems: { name: string; path: string }[] = [
     name: "Search",
     path: "/search",
   },
-];
+  {
+    name: "ỌpọlọAI",
+    path: "/OpoloAI",
+  },
+]
 
-export const protectedRoutes = ["/admin"];
+export const protectedRoutes = ["/admin"]
