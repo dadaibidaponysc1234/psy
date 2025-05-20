0d7b56dd0dae8e9366c7fdbc1626e1d6cdc5141d
Oladeji Omolabake <oladejiomolabake14@Gmail.com>
2025-05-20 10:46:21 +0100

    UI finished

--- src/app/(landing-routes)/layout.tsx
+++ src/app/(landing-routes)/layout.tsx
@@ -15,7 +15,7 @@ const LandingLayout = ({ children }: { children: ReactNode }) => {
   }, [])
 
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex flex-col">
       <NavBar mode={mode} />
       <div
         className={`flex ${pathname === "/OpoloAI" ? "h-full overflow-hidden" : "min-h-dvh"} flex-grow flex-col`}
